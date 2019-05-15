c7a726fed54b7344ab6d0c56e9dc2770fc8d3d81
François Déchelle <fdechelle@teclib.com>
2019-05-15 17:40:39 +0200

    update script

--- scripts/run-image.sh
+++ scripts/run-image.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SUPPORTED_DISTROS="ubuntu18.04 centos6.9 fedora29 debian9.6"
+
 function usage() {
     script=$(basename $0)
     echo >&2 "Usage: $script DISTRO"
@@ -7,20 +9,72 @@ function usage() {
     echo >&2 ""
     echo >&2 "DISTRO is the base Linux distro of the image"
     echo >&2 "Currently available distributions:"
-    echo >&2 "  - ubuntu18.04"
-    echo >&2 "  - centos6.9"
-    echo >&2 "  - fedora29"
-    echo >&2 "  - debian9.6"
+    for distro in $SUPPORTED_DISTROS ; do
+    echo >&2 "  - $distro"
+    done
     echo >&2 ""
     echo >&2 "Options:"
-    echo >&2 "-r ROOTDIR      give eyedb root dir (default: $HOME/eyedb"
+    echo >&2 "-r ROOTDIR      give eyedb root dir, where source dirs are located"
+    echo >&2 "                (default: \$HOME/eyedb)"
+    echo >&2 ""
     exit 1
+}
+
+function check_rootdir() {
+    for dir in eyedb eyedbsm ; do
+	if test ! -f "$1/$dir/configure.ac" ; then
+	    echo >&2 "Error: source directory \"$1/$dir\" not found!"
+	    exit 2
+	fi
+    done
+}
+
+function check_distro() {
+    for distro in $SUPPORTED_DISTROS ; do
+	if test "$distro" = "$1" ; then return ; fi
+    done
+    echo >&2 "Error: \"$1\" is not a supported distro!"
+    exit 2
+}
+
+function check_image() {
+    docker images | grep -qs "$1"
+}
+
+function check_image_exit() {
+    if ! check_image "$1" ; then
+	echo >&2 "Error: Docker image \"$1\" does not exists!"
+	exit 2
+    fi
+}
+
+function check_volume() {
+    docker volume ls | grep -qs "$1"
+}
+
+function check_volume_exit() {
+    if ! check_volume "$1" ; then
+	echo >&2 "Error: Docker volume \"$1\" does not exists!"
+	exit 2
+    fi
+}
+
+function create_volume() {
+    echo "Creating Docker volume \"$1\""
+    docker volume create $1
+}
+
+function create_volume_if_needed() {
+    if ! check_volume $1 ; then create_volume $1 ; fi
 }
 
 if [ $# -lt 1 ] ; then usage $* ; fi
 
-while getopts "h" opt; do
+while getopts "hr:" opt; do
     case $opt in
+	r)
+	    ROOTDIR="$OPTARG"
+	    ;;
 	h)
 	    usage
 	    ;;
@@ -34,17 +88,22 @@ while getopts "h" opt; do
 done
 shift $((OPTIND-1))
 
+if test -z "$ROOTDIR" ; then ROOTDIR="$HOME/eyedb" ; fi
+check_rootdir "$ROOTDIR"
+
 DISTRO=$1
+check_distro "$DISTRO"
+
 DISTRO_VOLUME=eyedb-${DISTRO}
-IMAGE=eyedb-${DISTRO}
+IMAGE=fdshell/eyedb:${DISTRO}
 
-. $(dirname $0)/funs.sh
-
-check_image_exit $IMAGE
+#check_image_exit $IMAGE
 
 create_volume_if_needed $DISTRO_VOLUME
 
-docker_run \
-    --mount source=eyedb-ssh,target=/root/.ssh \
-    --mount source=${DISTRO_VOLUME},target=/eyedb \
-    $IMAGE
+docker run --interactive --tty --rm \
+       --mount source=$DISTRO_VOLUME,target=/eyedb \
+       --mount type=bind,source=$HOME/projects/eyedb/git,target=/eyedb/git \
+       $IMAGE \
+       bash
+
